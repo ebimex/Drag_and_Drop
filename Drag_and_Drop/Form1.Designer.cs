3e1bf0d4f167cac1541228aa4fa96ba4958e2220
ebimex <ebimex65@gmail.com>
2022-05-21 22:59:41 +0200

    first commit

---
+++ Drag_and_Drop/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Drag_and_Drop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(725, 502);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 535);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Wyczyść";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 535);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kasuj linię 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(328, 535);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(409, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(218, 535);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Kasuj x linii";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 584);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kopiowanie tekstu metodą Przeciągnij i upuść";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Drag_and_Drop/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Drag_and_Drop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e) //w własciwościach TextBox przejść na Zdarzenia i wybrać DragEnter, posługuje się argumentem 'e'
+        {
+            e.Effect = DragDropEffects.Copy;   //pokazuje ale jeszcze nie dodaje do pola tekstowego (Copy-kopuje, Move - posi itd..)
+
+            textBox1.Text += e.Data.GetData(DataFormats.Text).ToString();   //teraz dodaje (+=) tekst do okna 'textBox1'
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)   //wykasuj zawsartoć okna
+        {
+            if (textBox1.Text != "")
+            {
+                textBox1.Text = "";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)      //skasuj linię tekstu
+        {
+            
+            string[] tablica_linii = { "aaa", "bbb", "ccc" };     //lub z góry określone znaki w tablicy
+            var nowe_linie = (from ln in tablica_linii select ln).SkipWhile(ln => ln != "aaa");
+            
+
+            foreach (var nowsze_linie in nowe_linie)
+            {
+                
+               // this.textBox1.Lines = nowsze_linie.ToArray();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] tablica_linii = this.textBox1.Lines;       //przepisz wszytkie linie do tablicy (array)
+            int ile_linii = 2;
+            var noweLinie = tablica_linii.Skip(ile_linii);  // kasuje ilość linii podanych w 'int ile_linii = 2;' zaczynając od początku
+            this.textBox1.Lines = noweLinie.ToArray();
+        }
+    }
+}
